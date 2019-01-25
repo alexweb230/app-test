6959a0946c7184a8622a7c2c4b415159f80734cb
tania <tani.zhelezniak@gmail.com>
2019-01-25 15:55:25 +0200

    first

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,30 @@
+import {Component, OnInit} from '@angular/core';
+import {Hero} from '../hero';
+import {HEROES} from '../mock-heroes';
+
+@Component({
+    selector: 'app-heroes',
+    templateUrl: './heroes.component.html',
+    styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+    selectedHero: Hero;
+
+    heroesLIst = HEROES;
+
+
+    onSelect(her: Hero): void{
+        this.selectedHero = her;
+    }
+
+
+
+
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
+
+}
