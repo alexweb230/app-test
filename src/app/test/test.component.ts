11e4dcaffb5c182b23096553fac25e18ff05d3fc
tania <tani.zhelezniak@gmail.com>
2019-02-19 17:07:09 +0200

    heroes

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestComponent implements OnInit {
 
+  displayName: boolean = false;
+    displayN: boolean = true;
+
   constructor() { }
 
   ngOnInit() {
